1baae6c115d28c671ec3f55fe3f27884a99881c4
unknown <fachatti mouataz billah>
2025-01-02 23:27:36 +0100

    diffrent types of declarations

--- varibales types.php
+++ varibales types.php
@@ -17,3 +17,18 @@
     echo $greet . '<br>';
     echo $price . '<br>';
     echo $score . '<br>';
+
+    echo gettype($completed) . '<br>'; // @gettype get the type of the variable
+    var_dump($completed) . '<br>'; //@var_dumb stops the code and print the variable and it value
+
+# 4 Compound Types
+    # array
+    $list = [1, 2, 3, a, b, c, -14, true];
+    print_r($list); // @print_r to pring an array
+    # object
+    # callable
+    # iterable
+
+# 2 Special Types
+    # ressources
+    # null
