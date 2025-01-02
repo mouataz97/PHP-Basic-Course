2a3199626dba7548b2850b12030d72cf344998fa
unknown <fachatti mouataz billah>
2025-01-02 23:05:17 +0100

    variables Declaration

---
+++ varibales types.php
@@ -0,0 +1,19 @@
+<?php
+
+/*D ata types & Type Casting */
+
+# 4 Scalar Types
+
+    # bool - True / False
+    $completed = true;
+    # int IN
+    $score = 75;
+    # float IR with vergules
+    $price = 8.99;
+    # string - "Mouataz", "Hello "
+    $greet = 'Hello Mouataz';
+
+    echo $completed . '<br>';
+    echo $greet . '<br>';
+    echo $price . '<br>';
+    echo $score . '<br>';
