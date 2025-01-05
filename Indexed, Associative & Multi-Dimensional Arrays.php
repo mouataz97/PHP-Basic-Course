130de8fa6c6fc39094e459766969b287b6f187f2
unknown <fachatti mouataz billah>
2025-01-05 21:24:04 +0100

    arrays

---
+++ Indexed, Associative & Multi-Dimensional Arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+// ARRAYS
+$programminglanguage1 = ["php","java","python"];
+
+$name = "mouataz";
+
+echo $programminglanguage1[1];
+
+echo '<pre>';
+    print_r($programminglanguage1); // print array
+echo '</pre>';
+
+echo count($programminglanguage1); // to count array length
+
+echo '<br>';
+
+array_push($programminglanguage1,'C#','C');
+
+echo '<br>';
+
+print_r($programminglanguage1); // print array
