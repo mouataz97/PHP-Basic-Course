d8742f719419a67f94a0e972e7f38ea789ecdf61
unknown <fachatti mouataz billah>
2025-01-05 22:01:18 +0100

    multidimensional array

--- Indexed, Associative & Multi-Dimensional Arrays.php
+++ Indexed, Associative & Multi-Dimensional Arrays.php
@@ -19,4 +19,29 @@ array_push($programminglanguage1,'C#','C');
 
 echo '<br>';
 
+$programminglanguage1 = [
+    'php' => '8.0',
+    'python' => '3.9',
+    'go' => '1.15'
+];
+
 print_r($programminglanguage1); // print array
+
+$programminglanguages = [   // multi dimensional array
+    'php' => '8.0',
+    'python' => [
+        'creator' => 'guido',
+        'extension' => '.py',
+        'website' => 'www.python.org',
+        'opensource' => True,
+        [
+            'version' => '3.9', 'releasedDate' => '10-5-2020'
+        ],
+        [
+            'version' => '3.8', 'releasedDate' => '10-14-2019'
+        ],
+    ],
+    'go' => '1.15'
+];
+
+echo $programminglanguages['python'][0]['releasedDate'];
