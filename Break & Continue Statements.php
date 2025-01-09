bca92397c4e511c6ef009f2e0c32debf5810ba2d
PHP course <mouatazbillah97@gmail.com>
2025-01-09 01:02:10 +0100

    creating diffrent loops

--- Break & Continue Statements.php
+++ Break & Continue Statements.php
@@ -1,1 +1,40 @@
 <?php
+
+$i = 1;
+
+# loops
+
+/** @var integer $i */
+
+for($i; $i <= 10; $i++){
+    echo " ".$i;
+}
+echo "<br>";
+
+# while
+
+while ($i < 10) {
+    echo " ". $i;
+    $i++;
+}
+echo "<br>";
+
+# do while
+
+do{
+    echo " ".$i;
+    $i++;
+}while($i <= 10);
+echo "<br>";
+
+# foreach
+
+foreach (range(1, 10) as $i) {
+    echo " ". $i;
+}
+echo "<br>";
+
+foreach(range(1, 10) as $i){
+    echo "<br>";
+    echo "hello number ". $i;
+}
