08af2a387f77401d59b3adef3e33861bd161900f
PHP course <mouatazbillah97@gmail.com>
2025-01-10 00:46:50 +0100

    match expression

--- else statement.php
+++ else statement.php
@@ -1,1 +1,31 @@
 <?php
+
+$payment = "cash";
+
+switch ($payment) {
+    case "cash":
+        echo "Payment is cash";
+        break;
+
+    case "credit":
+        echo "Payment is card";
+        break;
+
+    case "paypal":
+        echo "Payment is paypal";
+            break;
+
+    default:
+        echo "Payment is not valid";
+}
+
+#match expression
+
+$paymentstatus = 5;
+$paymentstatusDisplay = match($paymentstatus) {
+    print 1 => "Paid",
+    print 2,3 => "Declined",
+    print 0 => "Pending payment",
+    default => "Cancelled",
+};
+echo $paymentstatusDisplay;
