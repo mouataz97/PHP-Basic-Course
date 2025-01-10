261ab991d88d3fff3ddf5899d5e904ba78105da2
PHP course <mouatazbillah97@gmail.com>
2025-01-10 21:26:59 +0100

    include require

---
+++ include & required.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * require
+ * require_once
+ * include
+ * include_once
+ */
+
+include 'file.php';
+require_once 'file.php';
+$x++;
+
+echo $x . "<br>";
+
+// include_path
+
+echo 'Hello World';
+
+
+
+
+
+
+
+
