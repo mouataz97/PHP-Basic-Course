0743f0f73521434f5b2d00303cd4c59c9358ca01
PHP course <mouatazbillah97@gmail.com>
2025-01-11 22:06:56 +0100

    Functions

--- website/index.php
+++ website/index.php
@@ -6,7 +6,15 @@
     <title>Empty HTML</title>
 </head>
 <body>
-<?php include 'nav.php'; ?>
+<?php
+$nav = include 'nav.php';
+
+$nav = str_replace( 'about', 'About us', $nav);
+$nav = str_replace( 'contact', 'Contact us', $nav);
+$nav = str_replace( 'home', 'Home', $nav);
+
+echo $nav;
+?>
 
 
 </body>
