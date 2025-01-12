c9363f39c295eb45d2f0b3696fb36eddca18bf17
unknown <fachatti mouataz billah>
2025-01-12 23:26:57 +0100

    Anonymous, Callable, Closure & Arrow Functions

--- static variable.php
+++ static variable.php
@@ -1,1 +1,28 @@
 <?php
+
+global $y;
+$x = 3;
+
+function foo(){
+    global $x;
+    $GLOBALS['y'] = 10;
+    echo $x. ' ';
+}
+
+foo($x);
+echo $y;
+
+function getFunction() {
+    static $value = null;
+    if($value === null) {
+        $value = someFunction();
+    }
+}
+
+function someFunction(){
+    sleep(2);
+    echo ' Processing ';
+    return 10;
+}
+
+echo someFunction();
