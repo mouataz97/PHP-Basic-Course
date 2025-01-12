f48b9797eff0aaa20d4ea9f57bf72558863ad531
PHP course <mouatazbillah97@gmail.com>
2025-01-12 06:12:26 +0100

    arguments

--- Functions.php
+++ Functions.php
@@ -1,1 +1,19 @@
 <?php
+
+// function
+
+echo foo();
+echo bar();
+
+function foo(): void {
+    echo "Hello World <br>";
+    function bar(){
+        echo "bar";
+    }
+}
+
+function aVariable(): mixed {
+    return 1.8;
+}
+
+var_dump(aVariable());
