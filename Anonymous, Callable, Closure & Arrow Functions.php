4e49187258985f764dda1abeb4073732a5764114
unknown <fachatti mouataz billah>
2025-01-12 23:59:34 +0100

    callback function

--- Anonymous, Callable, Closure & Arrow Functions.php
+++ Anonymous, Callable, Closure & Arrow Functions.php
@@ -5,3 +5,17 @@
 function sum(int|float ...$numbers): int|float{
     return array_sum($numbers);
 }
+
+echo sum(12,23,-5);
+
+// call back function
+
+$array1 = [1, 2, 3];
+$array2 = array_map(function($element){
+    return $element * 2;
+}, $array1);
+
+echo '<pre>';
+print_r($array2);
+print_r($array1);
+echo '</pre>';
