fe9194f0ce9383dacf318a7f48c12fa470337cc0
PHP course <mouatazbillah97@gmail.com>
2025-01-12 07:11:54 +0100

    static variable

--- arguments.php
+++ arguments.php
@@ -1,1 +1,27 @@
 <?php
+
+/* function */
+
+// checks if the function arg and call not matching disallow it
+declare(strict_types=1);
+function foo($x, int|float $y = 10)
+{
+    return $x + $y;
+}
+
+echo foo(x:0.1,y: (float) '2');
+
+function maths(...$numbers): int
+{
+    //return array_sum($numbers);
+    $sum = 0;
+    foreach($numbers as $number){
+        $sum += $number;
+    }
+}
+echo maths(1, 2, 3, 4, 5);
+
+setcookie(name:'name', value: 'value', httponly: true);
+
+$arr = ['x' =>1,  'y'=>2, 5, 6, 7];
+echo foo(...$arr);
