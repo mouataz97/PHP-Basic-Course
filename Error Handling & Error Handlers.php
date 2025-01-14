527045becddbe648bfa8e8580fceeb09e94216de
PHP course <mouatazbillah97@gmail.com>
2025-01-15 00:05:16 +0100

    diffrent of error throwing when a problem accured

--- Error Handling & Error Handlers.php
+++ Error Handling & Error Handlers.php
@@ -1,5 +1,11 @@
 <?php
 
 // Error Handling
+// https://www.php.net/manual/en/errorfunc.constants.php
 
-error_reporting(0); // no errors will be reported
+//trigger_error( 'This is an error', E_USER_ERROR);// if problem = fatal error
+trigger_error('this is an error', E_USER_WARNING);//if problem = warrning error
+//error_reporting(0); // no errors will be reported
+
+echo 1;
+
