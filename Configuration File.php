434ea0ddb25f844b3a61207d318ef64413b7d918
PHP course <mouatazbillah97@gmail.com>
2025-01-14 23:53:53 +0100

    setting max execution time can be exceeded

--- Configuration File.php
+++ Configuration File.php
@@ -2,7 +2,7 @@
 
 // php ini & configuration
 // error_reporting, error_log, display_errors
-
+/*
 // var_dump(ini_get('display_errors'));
 //ini_set('error_reporting' ,E_ALL & ~E_NOTICE);
 var_dump(ini_get('display_errors'));
@@ -10,3 +10,11 @@ ini_set('display_errors' ,0);
 
 $array = [1];
 echo $array[2];
+*/
+var_dump(ini_get('display_errors'));
+ini_set('max_execution_time' , 3);
+
+sleep(5);
+echo 'Hello World';
+
+//var_dump(ini_get('memory_limit'));
