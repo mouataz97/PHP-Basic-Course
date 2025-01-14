97e17deca57e9febaaedcddd7416093c49c12351
PHP course <mouatazbillah97@gmail.com>
2025-01-14 23:49:50 +0100

    this error not fixed but become hidden

--- Configuration File.php
+++ Configuration File.php
@@ -3,9 +3,10 @@
 // php ini & configuration
 // error_reporting, error_log, display_errors
 
+// var_dump(ini_get('display_errors'));
+//ini_set('error_reporting' ,E_ALL & ~E_NOTICE);
 var_dump(ini_get('display_errors'));
-var_dump(E_ALL);
-ini_set('error_reporting' ,E_ALL & ~E_NOTICE);
+ini_set('display_errors' ,0);
 
 $array = [1];
 echo $array[2];
