fbbb1bde82df875c35ccc9d1e5240acf5087735f
PHP course <mouatazbillah97@gmail.com>
2025-01-14 23:42:52 +0100

    error_reporting

--- Configuration File.php
+++ Configuration File.php
@@ -4,3 +4,8 @@
 // error_reporting, error_log, display_errors
 
 var_dump(ini_get('display_errors'));
+var_dump(E_ALL);
+ini_set('error_reporting' ,E_ALL & ~E_NOTICE);
+
+$array = [1];
+echo $array[2];
