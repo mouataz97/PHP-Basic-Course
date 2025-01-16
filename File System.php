3175e4cd8730233430c209476e7a7af2ecc6b6a9
PHP course <mouatazbillah97@gmail.com>
2025-01-16 23:31:26 +0100

    creating checking and removing files

--- File System.php
+++ File System.php
@@ -1,1 +1,15 @@
 <?php
+
+// Working with file system
+
+$dir = scandir(__DIR__); //current PATH of the  file
+
+var_dump($dir);
+//mkdir('new_dir'); // to create a new directory
+//mrdir('new_dir'); // to remove it
+
+if(file_exists('file.txt')){
+    echo filesize('file.txt');
+}else{
+    echo "file not found";
+}
